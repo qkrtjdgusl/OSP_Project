11efd4235d4fab2166f6314fe88eab5118d0f021
rosaze <geenieeyoon@gmail.com>
2024-06-18 18:23:45 +0900

    add public: server files

---
+++ public/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+// Static files middleware to serve files from public folder
+app.use(express.static(path.join(__dirname)));
+
+// Route to serve the index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
